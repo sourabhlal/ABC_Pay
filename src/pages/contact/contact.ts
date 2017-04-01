dd059d0cbbe45a5f4979ea067737321e0f6f690d
Sourabh Lal <sourabhlal@gmail.com>
2017-04-02 01:06:18 +0200

    GRAAAAAAAPHS!

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
+  queries: {
+    canvas: new ViewChild('lineCanvas')
+ }
 })
 export class ContactPage {
+  
+  @ViewChild('lineCanvas') lineCanvas;
+  lineChart: any;
 
   constructor(public navCtrl: NavController) {
 
   }
+  
+  ionViewDidLoad() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+        type: 'line',
+        data: {
+            labels: ["03/30/2017", "03/31/2017", "04/01/2017"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [1,-2,7],
+                    spanGaps: false,
+                }
+            ]
+        }
+
+    });
+ }
+
 
 }
