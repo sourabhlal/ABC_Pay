2653dc0d765dcea7ae1616be35c356f8d33ce8b5
Sourabh Lal <sourabhlal@gmail.com>
2017-04-01 23:43:23 +0200

    added new tabs

---
+++ src/pages/targets/targets.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-targets',
+  templateUrl: 'targets.html'
+})
+export class TargetsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
