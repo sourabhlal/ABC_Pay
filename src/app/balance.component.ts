d0eb08d3c57d43c855dbb81aec0721b81a4841cf
Pandelis Zembashis <info@pandelis.me>
2017-04-01 17:27:21 +0100

    autorefresh

--- src/app/balance.component.ts
+++ src/app/balance.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map'
+import {Observable} from 'rxjs/Rx';
+
 
 @Component({
   selector: 'balance',
@@ -22,13 +24,18 @@ export class Balance {
 
   balance: string;
   constructor(private http:Http) {
-
-    http.get('https://mlhprime.pandelis.me/api/v1/balance')
+    
+    let timer = Observable.timer(0,5000);
+    timer.subscribe(t=> {
+        http.get('https://mlhprime.pandelis.me/api/v1/balance')
         // Call map on the response observable to get the parsed people object
         .map(res => res.json())
         // Subscribe to the observable to get the parsed people object and attach it to the
         // component
         .subscribe(balance => this.balance = balance.balance );
+    });
+
+ 
 
   }
 }
